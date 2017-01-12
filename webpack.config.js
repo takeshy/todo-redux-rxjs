0c8d77556921ab0519035894a4aba0aef2eaf21a
satotoru <satotoru2000@gmail.com>
2017-01-13 00:00:38 +0900

    fix basic settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,9 @@
-//const webpack = require('webpack');
-//module.exports = {
-//  entry: "./src/app.js",
-//  output: {
-//    path: __dirname + "/dist",
-//    filename: "bundle.js"
-//  },
-//  devServer: {
-//    contentBase: 'dist',
-//    inline: true,
-//    port: 8080
-//  },
-//  module: {
-//    loaders: [
-//      {
-//        test: /\.ejs$/,
-//        exclude: /node_modules/,
-//        loader: 'ejs-compiled-loader'
-//      },
-//      {
-//        test: /\.js$/,
-//        exclude: /node_modules/,
-//        loader: "babel-loader",
-//        query: {
-//          presets: ["es2015"]
-//        }
-//      }
-//    ]
-//  }
-//};
 var webpack = require('webpack');
-var path = require('path');
 var WebpackNotifierPlugin = require('webpack-notifier');
-var LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   entry: __dirname + "/ts/app.ts",
+  // entry: __dirname + "/src/app.js",
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js"
@@ -72,7 +41,6 @@ module.exports = {
       $: "jquery",
       Backbone: "backbone"
     }),
-    new LiveReloadPlugin(),
     new WebpackNotifierPlugin({})
   ]
 };
