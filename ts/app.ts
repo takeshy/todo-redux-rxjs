bf565f0d1019b163875f5f0a6622715d5f812e6e
Toru Sato <satotoru@users.noreply.github.com>
2017-01-13 11:24:29 +0900

    Fix basic settings (#1)
    * fix basic settings
    
    * add livereload

--- ts/app.ts
+++ ts/app.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="./typings/index.d.ts" />
 import 'babel-polyfill';
 import configureStore from './lib/configureStore';
 import { setDispatch, dispatch } from './lib/dispatch';
